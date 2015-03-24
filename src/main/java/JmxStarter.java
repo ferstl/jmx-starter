fb325237abea974484d896ba935f23353df03418
Stefan Ferstl <st.ferstl@gmail.com>
2015-03-24 22:48:18 +0100

    Run only on Oracle HotSpot 1.8 or greater

--- src/main/java/JmxStarter.java
+++ src/main/java/JmxStarter.java
@@ -6,10 +6,41 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 
-public class JmxStarter {
+public final class JmxStarter {
+
+  private static final String WRONG_JAVA_VERSION_FORMAT = "Java specification version 1.8 or greater is required. You are using '%s'";
+  private static final String WRONG_VM_FORMAT = "Oracle HotSpot VM is required. You are using '%s - %s'";
+
+  private JmxStarter() {}
 
   public static void main(String[] args) {
+    assertJavaVersion();
+    assertOracleHotspot();
     addToolsJarToClasspath();
+  }
+
+  private static void assertJavaVersion() {
+    String javaSpec = System.getProperty("java.specification.version", "0.0");
+    String[] versionParts = javaSpec.split("(\\d+)\\.(\\d+)(\\.*)?");
+
+    if (versionParts.length < 2) {
+      throw new IllegalStateException(String.format(WRONG_JAVA_VERSION_FORMAT, javaSpec));
+    }
+
+    int major = Integer.valueOf(versionParts[0]);
+    int minor = Integer.valueOf(versionParts[1]);
+    if (major < 1 && minor < 8) {
+      throw new IllegalStateException(String.format(WRONG_JAVA_VERSION_FORMAT, javaSpec));
+    }
+  }
+
+  private static void assertOracleHotspot() {
+    String javaVendor = System.getProperty("java.vendor", "unknown");
+    String vmName = System.getProperty("java.vm.name", "unknown");
+
+    if (!vmName.toLowerCase().contains("hotspot") || !javaVendor.toLowerCase().contains("oracle")) {
+      throw new IllegalStateException(String.format(WRONG_VM_FORMAT, javaVendor, vmName));
+    }
   }
 
   private static void addToolsJarToClasspath() {
