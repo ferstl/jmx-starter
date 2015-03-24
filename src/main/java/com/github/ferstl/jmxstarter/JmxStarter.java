85be868a042ad6627d64b17e2f6fa630a9ab1ee9
Stefan Ferstl <st.ferstl@gmail.com>
2015-03-24 23:43:17 +0100

    Cosmetics

--- src/main/java/com/github/ferstl/jmxstarter/JmxStarter.java
+++ src/main/java/com/github/ferstl/jmxstarter/JmxStarter.java
@@ -20,7 +20,7 @@ public final class JmxStarter {
 
   private static final Pattern JAVA_SPEC_VERSION_PATTERN = Pattern.compile("(\\d+)\\.(\\d+)(\\.*)?");
   private static final String WRONG_JAVA_VERSION_FORMAT = "Java specification version 1.8 or greater is required. You are using '%s'";
-  private static final String WRONG_VM_FORMAT = "Oracle HotSpot VM is required. You are using '%s - %s'";
+  private static final String WRONG_JVM_FORMAT = "Oracle HotSpot JVM is required. You are using '%s - %s'";
 
   private JmxStarter() {}
 
@@ -63,7 +63,7 @@ public final class JmxStarter {
     String vmName = System.getProperty("java.vm.name", "unknown");
 
     if (!vmName.toLowerCase().contains("hotspot") || !javaVendor.toLowerCase().contains("oracle")) {
-      throw new IllegalStateException(String.format(WRONG_VM_FORMAT, javaVendor, vmName));
+      throw new IllegalStateException(String.format(WRONG_JVM_FORMAT, javaVendor, vmName));
     }
   }
 
