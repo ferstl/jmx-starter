b555e87b7bc16e1a269e5a1e82b4db3e9823bd3f
Stefan Ferstl <st.ferstl@gmail.com>
2015-03-30 23:00:58 +0200

    Add test

---
+++ src/test/java/com/github/ferstl/jmxstarter/JmxStarterOptionsTest.java
@@ -0,0 +1,38 @@
+package com.github.ferstl.jmxstarter;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.Optional;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+public class JmxStarterOptionsTest {
+
+  private static final InputStream SYSTEM_IN_BACKUP = System.in;
+
+  @After
+  public void after() {
+    System.setIn(SYSTEM_IN_BACKUP);
+  }
+
+  @Test
+  public void defaults() {
+    setInput("4242");
+
+    Optional<JmxStarterOptions> optionsOptional = JmxStarterOptions.parse();
+    assertTrue(optionsOptional.isPresent());
+
+    JmxStarterOptions options = optionsOptional.get();
+    assertEquals(7091, options.jmxPort);
+    assertEquals(7091, options.rmiPort);
+    assertEquals("4242", options.pid);
+  }
+
+  private void setInput(String s) {
+    System.setIn(new ByteArrayInputStream(s.getBytes(Charset.defaultCharset())));
+  }
+}
