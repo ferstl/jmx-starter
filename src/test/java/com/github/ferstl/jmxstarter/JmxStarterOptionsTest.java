52cf0c27dbcd92d49b2e7ab4323cf86f7f14760c
Stefan Ferstl <st.ferstl@gmail.com>
2015-03-31 07:45:52 +0200

    Add test for long options

--- src/test/java/com/github/ferstl/jmxstarter/JmxStarterOptionsTest.java
+++ src/test/java/com/github/ferstl/jmxstarter/JmxStarterOptionsTest.java
@@ -2,10 +2,10 @@ package com.github.ferstl.jmxstarter;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.nio.charset.Charset;
 import java.util.Optional;
 import org.junit.After;
 import org.junit.Test;
+import static java.nio.charset.Charset.defaultCharset;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -45,12 +45,23 @@ public class JmxStarterOptionsTest {
   }
 
   @Test
+  public void nonDefaultsWithLongOptions() {
+    Optional<JmxStarterOptions> optionsOptional = JmxStarterOptions.parse("--jmx-port", "9998", "--rmi-port", "9999", "4242");
+    assertTrue(optionsOptional.isPresent());
+
+    JmxStarterOptions options = optionsOptional.get();
+    assertEquals(9998, options.jmxPort);
+    assertEquals(9999, options.rmiPort);
+    assertEquals("4242", options.pid);
+  }
+
+  @Test
   public void multiplePids() {
     Optional<JmxStarterOptions> optionsOptional = JmxStarterOptions.parse("4242", "4243");
     assertFalse(optionsOptional.isPresent());
   }
 
-  private void setInput(String s) {
-    System.setIn(new ByteArrayInputStream(s.getBytes(Charset.defaultCharset())));
+  private static void setInput(String s) {
+    System.setIn(new ByteArrayInputStream(s.getBytes(defaultCharset())));
   }
 }
