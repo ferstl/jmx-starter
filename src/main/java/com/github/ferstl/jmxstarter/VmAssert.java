cef550604096ae3f72f3f8b748a85a6d6703747c
Stefan Ferstl <st.ferstl@gmail.com>
2015-04-03 17:36:51 +0200

    Extract constants for system properties

--- src/main/java/com/github/ferstl/jmxstarter/VmAssert.java
+++ src/main/java/com/github/ferstl/jmxstarter/VmAssert.java
@@ -10,11 +10,14 @@ final class VmAssert {
   private static final Pattern JAVA_SPEC_VERSION_PATTERN = Pattern.compile("(\\d+)\\.(\\d+)(\\.*)?");
   private static final String WRONG_JAVA_VERSION_FORMAT = "Java specification version 1.8 or greater is required. You are using '%s'";
   private static final String WRONG_JVM_FORMAT = "Oracle HotSpot JVM is required. You are using '%s - %s'";
+  static final String JAVA_SPEC_VERSION_PROP = "java.specification.version";
+  static final String JAVA_VENDOR_PROP = "java.vendor";
+  static final String JAVA_VM_NAME_PROP = "java.vm.name";
 
   private VmAssert() {}
 
   static void assertJavaVersion(Properties systemProperties) {
-    String javaSpec = systemProperties.getProperty("java.specification.version", "0.0");
+    String javaSpec = systemProperties.getProperty(JAVA_SPEC_VERSION_PROP, "0.0");
     Matcher javaSpecMatcher = JAVA_SPEC_VERSION_PATTERN.matcher(javaSpec);
     if (!javaSpecMatcher.matches()) {
       throw new IllegalStateException(String.format(WRONG_JAVA_VERSION_FORMAT, javaSpec));
@@ -28,8 +31,8 @@ final class VmAssert {
   }
 
   static void assertOracleHotspot(Properties systemProperties) {
-    String javaVendor = systemProperties.getProperty("java.vendor", "unknown");
-    String vmName = systemProperties.getProperty("java.vm.name", "unknown");
+    String javaVendor = systemProperties.getProperty(JAVA_VENDOR_PROP, "unknown");
+    String vmName = systemProperties.getProperty(JAVA_VM_NAME_PROP, "unknown");
 
     if (!vmName.toLowerCase().contains("hotspot") || !javaVendor.toLowerCase().contains("oracle")) {
       throw new IllegalStateException(String.format(WRONG_JVM_FORMAT, javaVendor, vmName));
